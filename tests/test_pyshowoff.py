64241993ce8dc2b0f42a7da4cee1e4651da507a5
Aiden Nibali <dismaldenizen@gmail.com>
2017-10-21 00:18:14 +1100

    Test new_notebook

--- tests/test_pyshowoff.py
+++ tests/test_pyshowoff.py
@@ -12,3 +12,17 @@ class TestClient(unittest.TestCase):
             req = mock_send.call_args[0][0]
             self.assertEqual(req.method, 'GET')
             self.assertEqual(req.url, 'http://hostname:616/api/v2/notebooks')
+
+    def test_new_notebook(self):
+        client = Client('hostname:616')
+        with patch('pyshowoff.Client.request') as mock_request:
+            client.new_notebook('New notebook')
+            method, path, data = mock_request.call_args[0]
+            self.assertEqual(method, 'post')
+            self.assertEqual(path, '/api/v2/notebooks')
+            self.assertEqual(data, {
+                'data': {
+                    'type': 'notebooks',
+                    'attributes': {'title': 'New notebook'}
+                }
+            })
